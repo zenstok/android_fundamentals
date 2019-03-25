660531b23ecc8c2d588865d4cc83a178a6d01ac2
Rares <rares.tarabega@global-bit.com>
2019-03-26 00:17:52 +0200

    w2

---
+++ app/src/main/java/com/rares/android_fundamentals/W2.java
@@ -0,0 +1,13 @@
+package com.rares.android_fundamentals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class W2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_w2);
+    }
+}
